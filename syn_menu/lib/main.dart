2cbebbc8d4618b3a96756715a9e7fa3cbd6737cd
TrevorSatori <trevorsatori@gmail.com>
2023-03-22 14:49:26 -0500

    Satori's Commit

---
+++ syn_menu/lib/main.dart
@@ -0,0 +1,64 @@
+// main.dart
+import 'package:flutter/material.dart';
+
+import './settings.dart';
+import './home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+      title: "Syn-Menu",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedScreenIndex = 0;
+  final List _screens = [
+    // Change these strings for top Title
+    {"screen": const HomeScreen(), "title": "HomePage"},
+    {"screen": const SettingsScreen(), "title": "SettingsPage"}
+  ];
+
+  void _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(_screens[_selectedScreenIndex]["title"])),
+      ),
+      body: _screens[_selectedScreenIndex]["screen"],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedScreenIndex,
+        onTap: _selectScreen,
+        items: const [
+          // Label here will be shown as bottom navbar name
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
+        ],
+      ),
+    );
+  }
+}
---
+++ syn_menu/lib/settings.dart
@@ -0,0 +1,16 @@
+// screen_a.dart
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Settings Page',
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
