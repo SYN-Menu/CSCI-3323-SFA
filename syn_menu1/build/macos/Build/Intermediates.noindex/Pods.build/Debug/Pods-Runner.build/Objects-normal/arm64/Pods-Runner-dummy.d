74b7d2ea410c84198c75224012a64ae5615627d8
Toluwaez <91505730+Toluwaez@users.noreply.github.com>
2023-04-12 18:15:27 -0500

    commit

--- syn_menu1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/syn_menu1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/syn_menu1/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- syn_menu1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/syn_menu1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
-  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/syn_menu1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/DerivedSources/path_provider_foundation_vers.c
