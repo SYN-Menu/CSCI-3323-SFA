a9c5e44775a160577afd664f19777ec427d4d9c2
Toluwaez <91505730+Toluwaez@users.noreply.github.com>
2023-04-12 17:41:57 -0500

    the Up to date main

---
+++ SYN_Menu_Final/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/SYN_Menu_Final/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/SYN_Menu_Final/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
