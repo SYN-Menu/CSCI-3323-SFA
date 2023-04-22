c72813035a4469ea147948043ca21ed022ea8949
TrevorSatori <trevorsatori@gmail.com>
2023-04-21 20:51:58 -0500

    ensure input not malicious

--- SYN_Menu_Final/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/SYN_Menu_Final/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/tolu/Documents/Software\ Engineering(HW)/CSCI-3323-SFA/SYN_Menu_Final/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
