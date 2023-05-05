195d3f9b2f634732d352138f0a5a45bb95f61489
ReaganWix <122489115+ReaganWix@users.noreply.github.com>
2023-05-05 09:25:40 -0500

    Merge branch 'main' of https://github.com/SYN-Menu/CSCI-3323-SFA

--- SYN_Menu_Final/lib/structs/results.dart
+++ SYN_Menu_Final/lib/structs/results.dart
@@ -28,3 +28,53 @@ class Results extends StatelessWidget {
     );
   }
 }
+class DishInfo extends StatelessWidget{
+String name;
+List <String>? ingredients;
+DishInfo({required this.name, required this.ingredients});
+    String doStuff (List <String>? var1){
+      const string = '...';
+    try{
+      if(var1 != null){
+        for (int i = 0; i < var1.length; i++){
+          print(var1[i] + ", ");
+        }
+        return string;
+      }
+      else{
+        return string;
+      }
+    } catch (Exc) {
+      print(Exc);
+      rethrow;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff063F2C),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(children: [
+        ElevatedButton(
+          child: const Text("Return", style: TextStyle(fontSize: 25, color: Colors.white,),),
+          onPressed: (){Navigator.pop(context);},
+          style: ElevatedButton.styleFrom(primary: Colors.green.shade800),
+        ),
+            Column(
+              children: [ 
+              Text(name, style: TextStyle(fontSize: 25, color: Colors.white,),),
+              Text('Ingredients: ', style: TextStyle(fontSize: 25, color: Colors.white,),),
+              Text(doStuff(ingredients)),
+              ElevatedButton(
+                child: const Text("Favorite", style: TextStyle(fontSize: 25, color: Colors.white,),),
+                onPressed: () {  },
+                style: ElevatedButton.styleFrom(primary: Colors.green.shade800),
+              )
+          ])
+      ],),
+      )
+    );
+  }
+
+}
